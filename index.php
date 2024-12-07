67e5a56cf55dfd368cc54ed5f074265f0a91b021
Gabriel Carvalho <121682225+gabrielrenato1@users.noreply.github.com>
2024-12-07 17:55:50 -0300

    feat: abstraction of shopping cart

--- index.php
+++ index.php
@@ -4,4 +4,24 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use App\ShoppingCart;
 
-echo ShoppingCart::teste();
+$shoppingCart = new ShoppingCart();
+
+print_r($shoppingCart->getItems());
+echo "<br/> Valor total: " . $shoppingCart->getTotalValue() . '<br/>';
+
+$shoppingCart->addItem("Bolo de Cenoura", 15.90);
+$shoppingCart->addItem("Bolo de Chocolate", 10.90);
+$shoppingCart->addItem("Bolo de Doce de Leite", 20.90);
+
+print_r($shoppingCart->getItems());
+echo "<br/> Valor total: " . $shoppingCart->getTotalValue() . '<br/>';
+echo "Status: " . $shoppingCart->getStatus() . '<br/>';
+
+if($shoppingCart->confirmOrder()) {
+    echo "Pedido criado com sucesso <br/>";
+}else{
+    echo "Erro na confirmação do pedido. Carrinho sem itens <br/>";
+}
+
+echo "Status: " . $shoppingCart->getStatus() . '<br/>';
+
--- src/ShoppingCart.php
+++ src/ShoppingCart.php
@@ -4,8 +4,61 @@ namespace App;
 
 class ShoppingCart {
 
-    public static function teste(){
-        return 1;
+    private $items;
+    private $status;
+    private $totalValue;
+    
+    public function __construct(){
+        $this->items = [];
+        $this->status = "aberto";
+        $this->totalValue = 0.0;
     }
+    
+    public function getItems():array{
+        return $this->items;
+    }
+    
+    public function getTotalValue():float{
+        return $this->totalValue;
+    }
+    
+    public function getStatus():string{
+        return $this->status;
+    }
+
+    public function addItem(string $item, float $value):void{
+
+        $this->items[] = [
+            "item" => $item,
+            "value" => $value,
+        ];
+
+        $this->totalValue += $value;
+
+    }
+    
+    public function confirmOrder():bool{
+        
+        if($this->verifyCart()){
+            $this->status = "confirmado";
+            $this->sendConfirmOrderMail();
+            
+            return true;
+            
+        }
+        
+        return false;
+        
+    }
+    
+    public function sendConfirmOrderMail():void{
+        echo "<br/> Enviando email de confirmação <br/>";
+    }
+    
+    public function verifyCart():bool{
+        return !empty($this->items);
+    }
+    
+    
 
 }
